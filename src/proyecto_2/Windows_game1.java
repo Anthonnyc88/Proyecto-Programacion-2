70d05503c9686eb365d687711b520e78293f6b19
punts hola mundo <anthonnyc10@gmail.com>
2017-07-24 19:47:51 -0600

    Probando animaciones

--- src/proyecto_2/Windows_game1.java
+++ src/proyecto_2/Windows_game1.java
@@ -28,10 +28,30 @@ public class Windows_game1 extends javax.swing.JFrame {
     private void initComponents() {
 
         bntReturn = new javax.swing.JButton();
+        logica1 = new proyecto_2.Logica();
+        bntIniciar = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
         bntReturn.setText("Regresar");
+
+        javax.swing.GroupLayout logica1Layout = new javax.swing.GroupLayout(logica1);
+        logica1.setLayout(logica1Layout);
+        logica1Layout.setHorizontalGroup(
+            logica1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 358, Short.MAX_VALUE)
+        );
+        logica1Layout.setVerticalGroup(
+            logica1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 246, Short.MAX_VALUE)
+        );
+
+        bntIniciar.setText("Iniciar");
+        bntIniciar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                bntIniciarActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -39,19 +59,36 @@ public class Windows_game1 extends javax.swing.JFrame {
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
-                .addComponent(bntReturn)
-                .addContainerGap(309, Short.MAX_VALUE))
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(logica1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addContainerGap(30, Short.MAX_VALUE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(bntReturn)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addComponent(bntIniciar)
+                        .addGap(116, 116, 116))))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addContainerGap(264, Short.MAX_VALUE)
-                .addComponent(bntReturn)
+                .addContainerGap()
+                .addComponent(logica1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(bntReturn)
+                    .addComponent(bntIniciar))
                 .addContainerGap())
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void bntIniciarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_bntIniciarActionPerformed
+        // TODO add your handling code here:
+        logica1.inicio();
+      
+    }//GEN-LAST:event_bntIniciarActionPerformed
 
     /**
      * @param args the command line arguments
@@ -90,6 +127,8 @@ public class Windows_game1 extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton bntIniciar;
     private javax.swing.JButton bntReturn;
+    private proyecto_2.Logica logica1;
     // End of variables declaration//GEN-END:variables
 }
