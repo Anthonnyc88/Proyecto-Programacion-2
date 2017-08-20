59206b6b47584534d88e9d8afb8cdec831f56f6d
punts hola mundo <anthonnyc10@gmail.com>
2017-08-19 21:42:44 -0600

    Actualizando

--- src/proyecto_2/Windows_game2.java
+++ src/proyecto_2/Windows_game2.java
@@ -37,9 +37,14 @@ import java.awt.BorderLayout;
 import java.awt.Color;
 import java.util.Random;
 import java.util.ArrayList;
+import java.util.List;
 import javax.swing.JOptionPane;
 
 public class Windows_game2 extends JFrame implements ActionListener {
+
+    static List preguntas = new ArrayList();
+    static List respuestas = new ArrayList();
+    ArrayList<String> listaObstaculos = new ArrayList();
 
     public Windows_game2() {
         JToolBar barraHerramientas = new JToolBar();
@@ -94,9 +99,23 @@ public class Windows_game2 extends JFrame implements ActionListener {
         panel.updateUI();
     }
 
-    public void obstaculosRandom(BotonMatriz[][] botones) {
-        ArrayList<String> listaObstaculos = new ArrayList();
+    public void llenar_listas() {
+        String[] pre = {"Una variable puede ser nula?", "Una variable String puede ser convertidad a int?", "Una variable Int puede ser convertidad a String?", "Una variable String puede ser convertidad a double?",
+            "Una variable puede ser utilizada en otra clase si es privated?", "Una variable public se puede utulizar en otros metodos?", "Una variable protected puede ser modificada en otra clase?", "Una variable declarada public static es global?", "Una instancia es una variable?",
+            "Una instancia es una clase aparte?", "new clase(); es una instancia?", "Una variable int puede tener decimales?", "Se pueden sumar variables String?", "Una variable double puede tener numeros enteros?",
+            "Una variable long puede contener numeros de gran rango?", "Una interfaz grafica puede realizarse a codigo manual?", "Se puede caer un programa cuando un ciclo de iteracion se encicla ?", "Un caracter es lo mismo que una cadena de texto?",
+            "Los archivos almacenan informacion permanente?", "Las matrices almacenan todo tipo de dato?", "Las ArrayList tienen limites definidos?", "Las Listas tienen limited definidos?", "El println imprime de la misma forma que el print?",
+            "El /n hace lo mismo que el System.LineSeparetor()?", "El /n hace lo mismo que el  /r/n ?", "El % significa 'porciento'?", "En java se concatena con +?", "Se pueden sumar metodos de diferentes tipo?",
+            "Se puede retornar una variable en un metodo void?", "Format sirve para ordenar el codigo?", "Java es orientado a objetos?", "Una clase sin MAIN puede correr programas?", "Una forma de establecer el MAIN m?s rapido es escribiendo psvm?",
+            "Para establecer el System.out.println mas rapido es escribiendo sout?", "Para elevar una potencia en programacion se utiliza ^ ?", "Para cerrar una declaracion de cualquier tipo en java se utiliza {}?", "Se puede declarar una variable de cualquier tipo NULL?",
+            "Se puede declarar un metodo int con parametros String?", "Para ingresar informacion desde el teclado se utiliza la Clase Scanner?", "El BufferedReader se utiliza para la lectura de archivos?",};
+        //respuestas
+         String[] res = {"v", "v", "v", "f", "f", "v", "f", "v", "f", "f", "v", "f", "f", "f", "v", "v", "v", "f", "v", "f", "f", "v", "f", "v", "v", "f", "v", "f", "f", "v", "v", "f", "v", "v", "v", "v", "v", "v", "v", "v"};
 
+
+    }
+
+    public void obstaculosRandom(BotonMatriz[][] botones) {
         while (listaObstaculos.size() < 10) {
             int filaRandom = (int) (Math.random() * 5);
             int columnaRandom = (int) (Math.random() * 8);
@@ -104,10 +123,10 @@ public class Windows_game2 extends JFrame implements ActionListener {
 
             if (!(listaObstaculos.contains(posicion))) {
                 if (filaRandom == 0 && columnaRandom == 0) {
-                    // no hacer nada
+                    // no hacer nada no pinta la posicion 0
                 } else {
                     if (filaRandom == 4 && columnaRandom == 7) {
-                        // no hacer nada
+                        // no hacer nada no pinta la posicion donde termina al juego
                     } else {
                         listaObstaculos.add(posicion);
                         botones[filaRandom][columnaRandom].setBackground(Color.RED);
@@ -137,6 +156,7 @@ public class Windows_game2 extends JFrame implements ActionListener {
             }
 
         }
+
     }
 
     public static void main(String args[]) {
