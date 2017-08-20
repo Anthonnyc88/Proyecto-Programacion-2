ac9d7c52b31526552ace9439da154d9225970012
punts hola mundo <anthonnyc10@gmail.com>
2017-08-19 20:10:39 -0600

    Actualizando[JUEGOTAVO]

--- src/proyecto_2/Windows_game2.java
+++ src/proyecto_2/Windows_game2.java
@@ -23,6 +23,22 @@ import java.awt.BorderLayout;
 import java.awt.Color;
 import java.util.Random;
 
+import java.awt.BorderLayout;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JToolBar;
+import javax.swing.JTextField;
+import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.util.Random;
+import java.util.ArrayList;
+import javax.swing.JOptionPane;
+
 public class Windows_game2 extends JFrame implements ActionListener {
 
     public Windows_game2() {
@@ -32,7 +48,7 @@ public class Windows_game2 extends JFrame implements ActionListener {
         btnCrear = new JButton();
         panel = new JPanel(null);
         //Para que el JFrame cierre la aplicacion
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         btnCrear.setText("Cargar Matriz de Juego");
         btnCrear.addActionListener(this);
         barraHerramientas.add(btnCrear);
@@ -59,51 +75,55 @@ public class Windows_game2 extends JFrame implements ActionListener {
         //Se recorren las filas
         for (int fila = 0; fila < filas; fila++) {
 
-            for (int columna = 0; columna < columnas; columna++) 
-            {
+            for (int columna = 0; columna < columnas; columna++) {
                 botones[fila][columna] = new BotonMatriz(75 * columna, 100 * fila, 75, 100);
-
-                //////START OF THE GAME///////////////
-                if (fila == 0 && columna == 0) {
-                    botones[fila][columna].setName("INICIO");
-                    botones[fila][columna].setBackground(Color.yellow);
-                    botones[fila][columna].setNombre(fila, columna);
-                    
-                }
-                //Where the game ends
-                if (fila == 4 && columna == 7) {
-                    botones[fila][columna].setBackground(Color.GREEN);
-                    botones[fila][columna].setNombre(fila, columna);
-                   
-
-                }
-
-//                 //Random para generar el comodin
-//                int rand2 = (int) (Math.random() * 2);
-//                for (int y = 0; y < 5; y++) {
-//                    if (fila == rand2) {
-//                        botones[fila][columna].setBackground(Color.RED);
-//                    }
-//                }
-                
-                //Random de los osbtaculos
-                int rand = (int) (Math.random() * 6);
-                for (int x = 0; x < 5; x++) {
-                    if (fila == rand) {
-                        botones[fila][columna].setBackground(Color.MAGENTA);
-                    }
-                }
-
-                //The button is added to the panel
                 panel.add(botones[fila][columna]);
-                if (columna == 0 && fila == 0) {
-
-                }
-
             }
         }
+
+        //////START OF THE GAME///////////////
+        botones[0][0].setName("INICIO");
+        botones[0][0].setBackground(Color.YELLOW);
+        botones[0][0].setNombre(0, 0);
+        //Where the game ends
+        botones[4][7].setBackground(Color.GREEN);
+        botones[4][7].setNombre(4, 7);
+        //obstaculos        
+        obstaculosRandom(botones);
         //The panel is updated so that the buttons are visible
         panel.updateUI();
+    }
+    
+    public static void obstaculosRandom(BotonMatriz[][] botones){
+       ArrayList<String> listaObstaculos = new ArrayList();
+       
+       while (listaObstaculos.size() < 10) {
+            int filaRandom = (int) (Math.random() * 5);
+            int columnaRandom = (int) (Math.random() * 8);
+            String posicion = String.valueOf(filaRandom + "," + columnaRandom);
+
+            if (!(listaObstaculos.contains(posicion))) {
+                if(filaRandom == 0 && columnaRandom == 0){
+                    // no hacer nada
+                }else{
+                    if(filaRandom == 4 && columnaRandom == 7){
+                        // no hacer nada
+                    }else{
+                        listaObstaculos.add(posicion);
+                        botones[filaRandom][columnaRandom].setBackground(Color.RED);
+                        botones[filaRandom][columnaRandom].setNombre(filaRandom,columnaRandom);
+                        botones[filaRandom][columnaRandom].addActionListener(new ActionListener() {
+                            @Override
+                            public void actionPerformed(ActionEvent e) {
+                                JOptionPane.showMessageDialog(null, "Prueba de pregunta");
+                            }
+                        });
+                    }
+                }
+            }
+        }
+       
+       //return listaObstaculos;
     }
 
     public static void main(String args[]) {
